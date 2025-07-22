6aebec0473ebb85ab7a9249c082098bdffd89699
Mohamed Habib <byaalkhy43@gmail.com>
2025-07-23 02:08:29 +0300

    fix: remove vercel.json and comment out server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -10,7 +11,9 @@ export default defineConfig({
       plugins: [tailwindcss(), autoprefixer()],
     },
   },
-  server: {
-    historyApiFallback: true,
-  },
-});
+  // Solution Problem
+  // server: {
+  //   host: 'localhost', 
+  //   port: 3000, 
+  // },
+})
